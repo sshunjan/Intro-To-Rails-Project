61d82c0610dcd2e86b0b70cf061d7635c2846a21
Simranjeet Singh Hunjan <simranjeetsinghhunjan@gmail.com>
2018-10-11 14:54:35 -0500

    Added the Books controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
   get 'countries/index'
   get 'countries/show'
   resources :readers, only: [:index, :show]
   resources :countries, only: [:index, :show]
+  resources :books, only: [:index, :show]
 
   get 'about', to: 'pages#about', as: 'about'
   get 'description',to: 'pages#description',as: 'description'
